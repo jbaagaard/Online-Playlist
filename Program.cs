05883abf8fba2580092fae50dfb0fb3416cb824a
jbaagaard <teamtunfiske@gmail.com>
2019-01-28 02:08:07 +0100

    git

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PlaylistBackend
+{
+    class Program
+    {
+        static Server _server = new Server();
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine(RoomManager.AddRoom(new Room("room")));
+            Console.WriteLine(RoomManager.AddRoom(new Room("room")));
+                   
+            
+            
+            await _server.RunServer();
+        }
+    }
+}
